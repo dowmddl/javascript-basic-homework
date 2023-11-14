12ecd38efd168a337b17a2fff1da3bc0d38bb06e
dowmddl <mm501y@naver.com>
2023-11-14 13:26:54 +0900

    4회차 과제 완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,7 @@
 // 여기에 1번 문제의 답을 작성하세요.
-
+const getPosts = async () => {
+    const res = await fetch('https://dummyjson.com/products/add');
+    const data = await res.json();
+    return data;
+}
 // 여기에 2번 문제의 답을 작성하세요.
